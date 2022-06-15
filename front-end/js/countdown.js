63cd15dff59c8b66094f0f4dfa1d941c51dec7b7
Veeru <veeru.neerukonda@gmail.com>
2022-06-15 12:02:22 +0530

    the timer is now reseting correctly after the time has elapsed.

--- front-end/js/countdown.js
+++ front-end/js/countdown.js
@@ -2,30 +2,39 @@ let endTime = null;
 
 export function getTimeRemaining()
 {
-  if(endTime === null)
-    refreshEndTime();
+  function routine(){
+    if(endTime === null)
+      refreshEndTime();
 
-  const total = endTime - Date.parse(new Date());
-  const seconds = Math.floor( (total/1000) % 60 );
-  const minutes = Math.floor( (total/1000/60) % 60 );
-  const hours = Math.floor( (total/(1000*60*60)) % 24 );
-  const days = Math.floor( total/(1000*60*60*24) );
+    const total = endTime - Date.parse(new Date());
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
   
-  if(total === 0)
+  let timeRemaing = routine();
+  if(timeRemaing.total < 1000)
+  {
     endTime = null;
+    timeRemaing = routine();
+  }
 
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
+  return timeRemaing;
 }
 
 function refreshEndTime()
 {
     endTime = new Date().setDate(new Date().getDate() + 14);
+    // endTime = new Date().setMinutes(new Date().getMinutes() + 1);
 }
 
 export function setEndTime(timeRemaing)

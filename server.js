f354633521e1180cffa46a032373452fa8c590cf
Veeru <veeru.neerukonda@gmail.com>
2022-06-15 16:16:47 +0530

    fixed not binding to port bug in heroku.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const getTimeRemaining = require('./countdown.js');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = 3000 || process.env.port;
 
 app.use(express.static('front-end'));
 
